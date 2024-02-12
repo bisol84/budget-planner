cc38827d3484b01d560962de80cc8499e343ebc0
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-02-12 21:32:25 +0100

    code cleaning

--- public/js/accounts.js
+++ public/js/accounts.js
@@ -1,62 +1,59 @@
-// Get the accounts
+const accountsURL = 'http://localhost:3000/accounts'
+const addAccountBtn = document.getElementById('input-account-btn')
+
+// Display the accounts cards when page loads
 window.addEventListener('DOMContentLoaded', function() {
-  fetch('http://localhost:3000/accounts', {
+  fetch(accountsURL, {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json'
     }
   })
     .then(response => response.json())
-    .then(data => {
-        data.forEach(account => {
-          const accountId = account.ID;
-          const accountName = account.name
-          const accountDescription = account.description
-          const accountAmount = account.amount
-          const accountType = account.type
-            
-          const accountsDiv = document.getElementById('accounts');
-          const div = document.createElement('div');
-          div.innerHTML = `
-          <div class="w-full h-full bg-white shadow-lg rounded-lg p-5 dark:bg-slate-900">
-          <div class="flex items-center gap-x-4 mb-3">
-            <div class="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-              <svg class="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
-            </div>
-            <div class="flex-shrink-0">
-              <h3 class="block text-lg font-semibold text-gray-800 dark:text-white" id="account-name">${account.name}</h3>
-            </div>
-          </div>
-          <p class="text-gray-600 dark:text-gray-400">Montant : <span class="text-green-600" id="account-amount">${account.amount}</span></p>
-          <p class="text-gray-600 dark:text-gray-400" id="account-description">${account.description}</p>
-          <p class="text-gray-600 dark:text-gray-400" id="account-type">${account.type}</p>
-         </div>
-          `;
-          accountsDiv.appendChild(div);
-        });
-    })
+    .then(data => renderAccountsCards(data))
     .catch(error => {
-        console.error('Erreur lors de la réception des comptes :', error);
+        console.error('Error when rendering accounts cards :', error);
     })
 })
 
+// Create the account card
+function createAccountCard(account) {
+  const accountCard = document.createElement('div');
+  accountCard.innerHTML = `<div class="w-full h-full bg-white shadow-lg rounded-lg p-5 dark:bg-slate-900">
+    <div class="flex items-center gap-x-4 mb-3">
+      <div class="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
+        <svg class="flex-shrink-0 w-6 h-6 text-blue-600 dark:text-blue-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="13.5" cy="6.5" r=".5"/><circle cx="17.5" cy="10.5" r=".5"/><circle cx="8.5" cy="7.5" r=".5"/><circle cx="6.5" cy="12.5" r=".5"/><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"/></svg>
+      </div>
+      <div class="flex-shrink-0">
+        <h3 class="block text-lg font-semibold text-gray-800 dark:text-white" id="account-name">${account.name}</h3>
+      </div>
+    </div>
+    <p class="text-gray-600 dark:text-gray-400">Montant : <span class="text-green-600" id="account-amount">${account.amount}</span></p>
+    <p class="text-gray-600 dark:text-gray-400" id="account-description">${account.description}</p>
+    <p class="text-gray-600 dark:text-gray-400" id="account-type">${account.type}</p>
+    </div>`
+    return accountCard
+}
+
+// Render all accounts cards
+function renderAccountsCards(accounts) {
+  const accountsDiv = document.getElementById('accounts')
+  accounts.forEach(account => {
+    const accountCard = createAccountCard(account)
+    accountsDiv.appendChild(accountCard)
+  })
+}
+
 // Add account
-const addAccountBtn = document.getElementById('input-account-btn')
-
 addAccountBtn.addEventListener('click', function() {  
-  const accountName = document.getElementById('input-account-name').value
-  const accountDescription = document.getElementById('input-account-description').value
-  const accountAmount = document.getElementById('input-account-amount').value
-  const accountType = document.getElementById('input-account-type').value
-
   const jsonData = {
-    accountName: accountName,
-    accountDescription: accountDescription,
-    accountAmount: accountAmount,
-    accountType: accountType
+    accountName: document.getElementById('input-account-name').value,
+    accountDescription: document.getElementById('input-account-description').value,
+    accountAmount: document.getElementById('input-account-amount').value,
+    accountType: document.getElementById('input-account-type').value
   }
 
-  fetch('http://localhost:3000/accounts', {
+  fetch(accountsURL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -65,9 +62,9 @@ addAccountBtn.addEventListener('click', function() {
   })
     .then(response => response.json())
     .then(data => {
-      
+      // Check the data
     })
     .catch(error => {
-      console.error('Une erreur s\'est produite:', error);
+      console.error('Error when saving account:', error);
       })
 })
--- public/js/budgets.js
+++ public/js/budgets.js
@@ -1,4 +1,102 @@
-// Get the budgets categories, budgets and transaction statuts
+// Create a budget line
+function createBudgetTableLine(budgetTable) {
+  const tr = document.createElement('tr');
+  budgetTable.appendChild(tr);
+  return tr
+}
+
+// Create an element with classes
+function createElementWithClasses(tagName, classNames) {
+  const element = document.createElement(tagName);
+  classNames.forEach(className => element.classList.add(className));
+  return element;
+}
+
+// Create a new cell in budget table
+function createTableCell(budgetLine) {
+  const td = createElementWithClasses('td', ['h-px', 'w-px', 'whitespace-nowrap']);
+  const div = createElementWithClasses('div', ['px-6', 'py-2'])
+  td.appendChild(div);
+  budgetLine.appendChild(td);
+  return div;
+}
+
+// Add text content to cell
+function addTextContent(div, value) {
+  const span = createElementWithClasses('span', ['text-sm', 'text-gray-600', 'dark:text-gray-400']);
+  span.textContent = value;
+  div.appendChild(span);
+}
+
+// Add numeric content to cell
+function addNumericContent(div, value) {
+  const span = createElementWithClasses('span', ['text-sm', 'text-gray-600', 'dark:text-gray-400']);
+  if (value == null) {
+    span.textContent = '0.00'
+  } else {
+    const numericValue = parseFloat(value);
+    span.textContent = numericValue.toFixed(2);
+  }
+  div.appendChild(span);
+}
+
+// Add numeric content with color balance to cell
+function addNumericContentWithColor(div, value) {
+  const span = createElementWithClasses('span', ['text-sm', 'text-gray-600', 'dark:text-gray-400']);
+  if (value == null) {
+    span.textContent = '0.00'
+  } else {
+    const numericValue = parseFloat(value);
+    span.textContent = numericValue.toFixed(2);
+  }
+
+  if (value == 0) {
+    span.classList.add('text-gray-600')
+  } else if (value < 0) {
+    span.classList.add('text-red-600')
+    span.classList.remove('text-gray-600', 'dark:text-gray-400')
+  } else {
+    span.classList.add('text-green-600')
+    span.classList.remove('text-gray-600', 'dark:text-gray-400')
+  }
+  div.appendChild(span);
+}
+
+// Add button to cell
+function addModifyButton(div, categoryId) {
+  const button = createElementWithClasses('button', ['inline-flex', 'items-center', 'gap-x-1', 'text-sm', 'text-blue-600', 'decoration-2', 'hover:underline', 'font-medium', 'dark:focus:outline-none', 'dark:focus:ring-1', 'dark:focus:ring-gray-600']);
+  button.textContent = 'Modifier';
+  button.id = categoryId;
+  button.onclick = function(e) {
+    editBudget(categoryId)
+  }
+  div.appendChild(button);
+}
+
+// Display budget table
+function displayBudgetTable(budgets) {
+  const budgetTable = document.getElementById('budget-table');
+  budgets.forEach(budget => {
+    const budgetLine = createBudgetTableLine(budgetTable)
+    const divBudgetCategory = createTableCell(budgetLine)
+    addTextContent(divBudgetCategory, budget.category)
+    const divBudgetAmount = createTableCell(budgetLine)
+    addNumericContent(divBudgetAmount, budget.amount)
+    const divBudgetTransactionsAmount = createTableCell(budgetLine)
+    addNumericContent(divBudgetTransactionsAmount, budget.transactions_amount)
+
+    // Special because of calculation
+    const amountLeft = Math.sign(budget.transactions_amount) === Math.sign(budget.amount) ? budget.transactions_amount - budget.amount : budget.transactions_amount + budget.amount;
+    const divBudgetAmountLeft = createTableCell(budgetLine)
+    addNumericContentWithColor(divBudgetAmountLeft, amountLeft)
+
+    // Special because of button onClick event
+    const divBudgetSaveButton = createTableCell(budgetLine)
+    addModifyButton(divBudgetSaveButton, budget.ID)
+  })
+}
+
+// Display the budgets categories, the amount and the transaction amount when page loads
 window.addEventListener('DOMContentLoaded', function() {
   fetch('http://localhost:3000/budgets/', {
     method: 'GET',
@@ -7,117 +105,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   })
     .then(response => response.json())
-    .then(data => {
-        data.forEach(budget => {
-          const categoryId = budget.ID
-          const budgetCategory = budget.category;
-          const budgetAmount = budget.amount || 0
-          const budgetPeriod = budget.period
-          const budgetTransactions = budget.transactions_amount || 0
-            
-          const budgetTable = document.getElementById('budget-table');
-          
-          // Créer un nouvel élément tr
-          const tr = document.createElement('tr');
-
-          // Créer un nouvel élément td
-          const tdCategory = document.createElement('td');
-          tdCategory.classList.add('h-px', 'w-px', 'whitespace-nowrap');
-
-          // Créer un nouvel élément div
-          const divCategory = document.createElement('div');
-          divCategory.classList.add('px-6', 'py-2');
-
-          // Créer un nouvel élément span
-          const spanCategory = document.createElement('span');
-          spanCategory.classList.add('text-sm', 'text-gray-600', 'dark:text-gray-400');
-          spanCategory.textContent = budgetCategory;
-
-          // Ajouter le span au div
-          divCategory.appendChild(spanCategory);
-
-          // Ajouter le div au td
-          tdCategory.appendChild(divCategory);
-
-          // Ajouter le td au tr
-          tr.appendChild(tdCategory);
-
-          // Ajouter le tr au conteneur
-          budgetTable.appendChild(tr);
-
-          // Créer un nouvel élément td
-          const tdBudget = document.createElement('td');
-          tdBudget.classList.add('h-px', 'w-px', 'whitespace-nowrap');
-
-          // Créer un nouvel élément div
-          const divBudget = document.createElement('div');
-          divBudget.classList.add('px-6', 'py-2');
-
-          // Créer un nouvel élément span
-          const spanBudget = document.createElement('span');
-          spanBudget.classList.add('text-sm', 'text-gray-600', 'dark:text-gray-400');
-          spanBudget.textContent = budgetAmount;
-
-          // Ajouter le span au div
-          divBudget.appendChild(spanBudget);
-
-          // Ajouter le div au td
-          tdBudget.appendChild(divBudget);
-
-          // Ajouter le td au tr
-          tr.appendChild(tdBudget);
-
-          // Ajouter le tr au conteneur
-          budgetTable.appendChild(tr);
-
-          // Transactions
-          const tdTransactions = document.createElement('td');
-          tdTransactions.classList.add('h-px', 'w-px', 'whitespace-nowrap');
-          const divTransactions = document.createElement('div');
-          divTransactions.classList.add('px-6', 'py-2');
-          const spanTransactions = document.createElement('span');
-          spanTransactions.classList.add('text-sm', 'text-gray-600', 'dark:text-gray-400');
-          spanTransactions.textContent = budgetTransactions.toFixed(2);
-          divTransactions.appendChild(spanTransactions);
-          tdTransactions.appendChild(divTransactions);
-          tr.appendChild(tdTransactions);
-          budgetTable.appendChild(tr);
-          
-          // Restant
-          const tdAmountLeft = document.createElement('td');
-          tdAmountLeft.classList.add('h-px', 'w-px', 'whitespace-nowrap');
-          const divAmountLeft = document.createElement('div');
-          divAmountLeft.classList.add('px-6', 'py-2');
-          const spanAmountLeft = document.createElement('span');
-          spanAmountLeft.classList.add('text-sm');
-          const amountLeft = Math.sign(budgetTransactions) === Math.sign(budgetAmount) ? budgetTransactions - budgetAmount : budgetTransactions + budgetAmount;
-          if (amountLeft == 0) {
-            spanAmountLeft.classList.add('text-gray-600')
-          } else if(amountLeft < 0) {
-            spanAmountLeft.classList.add('text-red-600')
-          } else {
-            spanAmountLeft.classList.add('text-green-600')
-          }
-          spanAmountLeft.textContent = amountLeft.toFixed(2);
-          divAmountLeft.appendChild(spanAmountLeft);
-          tdAmountLeft.appendChild(divAmountLeft);
-          tr.appendChild(tdAmountLeft);
-          budgetTable.appendChild(tr);
-
-          const editCell = document.createElement('td');
-          const editButton = document.createElement('button');
-          editButton.textContent = 'Modifier';
-          editButton.href = '#'; 
-          editButton.id = categoryId;
-          editButton.classList.add('inline-flex', 'items-center', 'gap-x-1', 'text-sm', 'text-blue-600', 'decoration-2', 'hover:underline', 'font-medium', 'dark:focus:outline-none', 'dark:focus:ring-1', 'dark:focus:ring-gray-600');
-          editCell.appendChild(editButton);
-          editCell.classList.add('h-px', 'w-px', 'whitespace-nowrap', 'px-6', 'py-1.5');
-          editButton.onclick = function(e) {
-            editBudget(categoryId)
-          }
-          tr.appendChild(editCell);
-        });
-    })
+    .then(data => displayBudgetTable(data))
     .catch(error => {
         console.error('Erreur lors de la réception des budgets :', error);
     })
@@ -128,14 +116,14 @@ function editBudget(categoryId) {
   const editMmodal = document.getElementById('edit_modal');
   editMmodal.classList.remove('hidden');
   const btnCloseModal = document.getElementById('close-modal')
-  btnCloseModal.addEventListener('click', function () {
+  btnCloseModal.addEventListener('click', function (e) {
+    e.preventDefault()
     saveBudget(categoryId)
   })
 }
 
 function saveBudget(categoryId) {
   const amount = document.getElementById('input-account-amount').value
-  console.log(amount)
   const editMmodal = document.getElementById('edit_modal');
   editMmodal.classList.add('hidden');
   const jsonData = {}
@@ -148,9 +136,9 @@ function saveBudget(categoryId) {
             }
         })
         .then(response => response.json())
-        .then(data => {
+        /*.then(data => {
   
-        })
+        })*/
         .catch(error => {
             console.error('Erreur lors de l\'envoi du JSON au serveur:', error);
         });
