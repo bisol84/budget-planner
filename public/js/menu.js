06e9ab818c97a175b0fd77af854926ce75d0a498
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-02-17 17:13:11 +0100

    modal correction

--- public/js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('../html/menu.html')
-        .then(response => response.text())
-        .then(menuContent => {
-            document.getElementById('menu').innerHTML = menuContent
-        })
-        .catch(error => console.error('Error fetching menu:', error))
-});
