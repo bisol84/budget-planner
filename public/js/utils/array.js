c51dfcee8c4008c97d6ae94cf17a645979aaec5f
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-03-04 21:04:02 +0100

    Comment cleaning

--- public/js/utils/array.js
+++ public/js/utils/array.js
@@ -1,34 +1,54 @@
-// Create table line
+/**
+ * Create table line
+ * @param {*} table 
+ * @returns 
+ */
 export function createTableLine(table) {
     const tr = createElementWithClasses('tr', ['odd:bg-white', 'even:bg-gray-50', 'border-b']);
     table.appendChild(tr);
     return tr
   }
 
-// Create sub header table line
+/**
+ * Create sub header table line
+ * @param {*} table 
+ * @returns 
+ */
 export function createTableSubHeaderLine(table) {
   const tr = createElementWithClasses('tr', ['bg-gray-200', 'rounded-md', 'px-3', 'py-2', 'font-bold']);
   table.appendChild(tr);
   return tr
 }
 
-// Create an element with classes
+/**
+ * Create an element with classes
+ * @param {*} tagName 
+ * @param {*} classNames 
+ * @returns 
+ */
 export function createElementWithClasses(tagName, classNames) {
     const element = document.createElement(tagName);
     classNames.forEach(className => element.classList.add(className));
     return element;
 }
   
-// Create a new cell in budget table
+/**
+ * Create a new cell in budget table
+ * @param {*} budgetLine 
+ * @returns 
+ */
 export function createTableCell(budgetLine) {
   const td = createElementWithClasses('td', ['px-3', 'py-2']);
-  //const div = createElementWithClasses('div', ['px-6', 'py-3'])
-  //td.appendChild(div);
   budgetLine.appendChild(td);
   return td;
 }
 
-// Add text content to cell
+/**
+ * Add text content to cell
+ * @param {*} div 
+ * @param {*} value 
+ * @returns 
+ */
 export function addTextContent(div, value) {
   const span = createElementWithClasses('span', ['text-sm', 'text-gray-600', 'dark:text-gray-400']);
   span.textContent = value;
@@ -36,7 +56,11 @@ export function addTextContent(div, value) {
   return span
 }
   
-// Add numeric content to cell
+/**
+ * Add numeric content to cell
+ * @param {*} div 
+ * @param {*} value 
+ */
 export function addNumericContent(div, value) {
   const span = createElementWithClasses('span', ['text-sm', 'text-gray-600', 'dark:text-gray-400']);
   if (value == null) {
@@ -48,7 +72,11 @@ export function addNumericContent(div, value) {
   div.appendChild(span);
 }
   
-// Add numeric content with color balance to cell
+/**
+ * Add numeric content with color balance to cell
+ * @param {*} div 
+ * @param {*} value 
+ */
 export function addNumericContentWithColor(div, value) {
   const span = createElementWithClasses('span', ['text-sm', 'text-gray-600', 'dark:text-gray-400']);
   if (value == null) {
@@ -70,7 +98,13 @@ export function addNumericContentWithColor(div, value) {
   div.appendChild(span);
 }
 
-// Add Tag to cell
+/**
+ * Add Tag to cell
+ * @param {*} div 
+ * @param {*} color 
+ * @param {*} value 
+ * @returns 
+ */
 export function addTag(div, color, value) {
   const bgcolor = color
   const txtcolor = getTextColor(color)
@@ -80,10 +114,13 @@ export function addTag(div, color, value) {
   return span
 }
 
-// Add icon to cell
+/**
+ * Add icon to cell
+ * @param {*} div 
+ * @param {*} icons 
+ */
 export function addIcon(div, icons) {
   const i = document.createElement('i');
-  // Split space class
   if (icons) {
     const iconSplit = icons.split(' ')
     iconSplit.forEach(icon => {
@@ -93,7 +130,13 @@ export function addIcon(div, icons) {
   div.appendChild(i);
 }
 
-// Add button to cell
+/**
+ * Add button to cell
+ * @param {*} div 
+ * @param {*} textContent 
+ * @param {*} categoryId 
+ * @returns 
+ */
 export function addButton(div, textContent,categoryId) {
   const button = createElementWithClasses('button', ['inline-flex', 'items-center', 'gap-x-1', 'text-sm', 'text-gray-600', 'decoration-2', 'hover:underline', 'font-medium', 'dark:focus:outline-none', 'dark:focus:ring-1', 'dark:focus:ring-gray-600']);
   button.textContent = textContent;
@@ -102,7 +145,11 @@ export function addButton(div, textContent,categoryId) {
   return button
 }
 
-// Check if color is dark
+/**
+ * If color is dark, text color must be white
+ * @param {*} hexColor 
+ * @returns 
+ */
 function getTextColor(hexColor) {
   if (hexColor) {
     const decimalColor = parseInt(hexColor.replace(/^#/, ''), 16);
