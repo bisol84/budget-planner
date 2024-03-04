3291274d39067db7fffb19f21e6d4151421962a4
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-03-04 18:18:05 +0100

    Security : add limiter
    Fix code scanning alert - Missing rate limiting #28

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-// Server file for budget-planner apop
+// Server file for budget-planner app
 const express = require('express')
 const cors = require('cors');
+const rateLimiter = require('express-rate-limit')
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -8,6 +9,16 @@ const PORT = 3000
 
 // App express
 const app = express ()
+
+// Security - Limiter
+const limiter = rateLimiter.rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+app.use(limiter)
 
 // Middleware
 const corsOptions = {
