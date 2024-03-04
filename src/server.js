bd3fb0deaff772ea1e4c4fb40fa2ed5fb125de19
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-03-04 18:26:21 +0100

    Comment cleaning

--- src/server.js
+++ src/server.js
@@ -16,7 +16,6 @@ const limiter = rateLimiter.rateLimit({
 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
 })
 app.use(limiter)
 
