deba16fe7dbc71b60048985b1563f01c707686bc
Sebastien Bisoglio <sbisoglio@gmail.com>
2024-03-19 19:17:30 +0100

    Dashboard preparation

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,56 +1,68 @@
-const dashboardMonth = document.getElementById('dashboard-month')
-
-// Display the expenses / income for the current month by default
-window.addEventListener('DOMContentLoaded', function() {
-
-})
-
 /**
- * Return percentage
- * @param {*} partialValue 
- * @param {*} totalValue 
- * @returns 
- */
-function percentage(partialValue, totalValue) {
-  return Math.round((100 * partialValue) / totalValue);
-} 
-
-/**
- * Create and return an total transactions / budgets
+ * Create and return the budget card
  * @param {*} total 
  * @returns 
  */
-function createTotalCard(data) {
-  const totalDiv = document.getElementById('total')
-  const totalCard = document.createElement('div');
-  const budgetsAmount = data[0].budgets_amount
-  const transactionsAmount = data[0].transactions_amount
-  const percentageUsed = percentage(transactionsAmount, budgetsAmount)
-  totalCard.innerHTML = `
+function createBudgetCard(totalBudget, totalTransactions) {
+  const budgetDiv = document.getElementById('budget')
+  const budgetCard = document.createElement('div');
+  // const percentageUsed = percentage(transactionsAmount, budgetsAmount)
+  budgetCard.innerHTML = `
   <div class="box-small">
     <div class="box-content">
       <p class="text-title">Total</p>
-      <p class="text-content">Budgets : ${budgetsAmount}</p>
-      <p class="text-content">Transactions : ${transactionsAmount}</p>
-      <div class="text-subcontent">${percentageUsed}%</div>
+      <p class="text-content">Total : ${totalBudget - totalTransactions} CHF</p>
+      <span class="text-subcontent">Total budget : ${totalBudget} CHF</span> - 
+      <span class="text-subcontent">Total transactions : ${totalTransactions} CHF</span>
     </div>
   </div>
     `
-    totalDiv.appendChild(totalCard)
+  budgetDiv.appendChild(budgetCard)
 }
 
-/**
- * Get amount of budget / transaction for selected month
- */
-function getBudget() {
-  fetch(`/api/dashboard/totalAmounts/`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      createTotalCard(data)
-    })
+async function displayBudgetInformation() {
+  try {
+    const totalBudget = await getBudgetTotal();
+    const totalTransactions = await getTransactionsTotal();
+    console.log(totalTransactions)
+    createBudgetCard(totalBudget, totalTransactions);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  displayBudgetInformation();
+});
+
+async function getBudgetTotal() {
+  try {
+    const response = await fetch(`/api/dashboard/totalBudget`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    return data[0].total_budget;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+async function getTransactionsTotal() {
+  try {
+    const response = await fetch(`/api/dashboard/totalTransactions`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    return data[0].total_transactions;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
 }
--- src/routes/routes-dashboard.js
+++ src/routes/routes-dashboard.js
@@ -1,21 +1,38 @@
 const router = require("express").Router();
 const db = require('../database/db');
 
-// Return the total budget and transactions for given month
-router.get("/totalAmounts/:date/", function (req, res) {
-  const monthFilter = req.params.date
+// Return the total budget for the current month
+router.get("/totalBudget/", function (req, res) {
   let sql = `
   SELECT 
-    -- SUM(CASE WHEN c.category = 'A classer' AND t.amount > 0 THEN -t.amount ELSE t.amount END) AS "transactions_amount",
-    SUM(t.amount) AS "transactions_amount",
-    SUM(b.amount) AS "budgets_amount"
-  FROM categories c
-  LEFT OUTER JOIN budgets b on b.id_category = c.ID and b.start_date <= ? and b.end_date > ?
-  LEFT OUTER JOIN transactions t on t.id_category = c.ID and strftime('%m', t.date) = strftime('%m', ?) and strftime('%Y', t.date) = strftime('%Y', ?)
+    COALESCE(SUM(b.amount), 0) AS total_budget
+  FROM budgets b
+  WHERE 
+    b.start_date <= strftime('%Y-%m', 'now') AND b.end_date >= strftime('%Y-%m', 'now')
   `;
-  const params = [monthFilter, monthFilter, monthFilter, monthFilter];
   const response = [];
-  db.all(sql, params, (err, rows) => {
+  db.all(sql, (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      response.push(row);
+    });
+    res.send(response)
+  });
+});
+
+// Return the total budget for the current month
+router.get("/totalTransactions/", function (req, res) {
+  let sql = `
+  SELECT 
+    COALESCE(SUM(t.amount), 0) AS total_transactions
+  FROM transactions t
+  WHERE 
+  strftime('%Y-%m', t.date) = strftime('%Y-%m', 'now')
+  `;
+  const response = [];
+  db.all(sql, (err, rows) => {
     if (err) {
       throw err;
     }
